85e4e8b4a7bf67df95bb110b274ea3cb9a77b454
Sam Graham <samgraham@outlook.com>
2015-02-06 20:33:10 -0700

    Added in more checks and fixed not including the query

--- todo.php
+++ todo.php
@@ -1,12 +1,14 @@
 <?php
 	echo "Php!";
 	require 'dbConnection.php';
+	require 'queries.php';
 	$db = loadDatabase();
 	echo $db;
 	$stmt = $db->query($q_getAll);
 	$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
         
 	foreach ($results as $temp) {
+		echo $temp;
 		foreach($temp as $temp1) {
 			      echo $temp1;
 		}
