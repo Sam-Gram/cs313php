e45cbba7c4934aae408597bc1261b2adbd355c48
Samuel-Graham <samgraham@outlook.com>
2015-02-07 22:45:08 -0700

    Added in date time conditional formatting

--- todo.php
+++ todo.php
@@ -10,12 +10,23 @@
 </head>
 <body class="container">
 <?php
+    function printDueDate($date) {
+        $php_date = date_create_from_format('Y-m-d', $date);
+
+        if ($php_date) {
+            print "<td style=" . (date_diff($php_date , (new DateTime()))->invert ? 'background-color:green;' : 'background-color:red;') . ">" . ($date !== null ? $date : 'None Set') . '</td>';
+        }
+        else {
+            print "<td>" . ($date !== null ? $date : 'None Set') . '</td>';
+        }
+
+    }
+
 	error_reporting(E_ALL);
 	echo "<h1>Php!</h1>";
 	require 'dbConnection.php';
 	require 'queries.php';
 	$db = loadDatabase();
-//    echo $q_getAll;
 	$stmt = $db->query($q_getAll);
 
     print '<table class="table table-hover">';
@@ -30,7 +41,7 @@
         print '<td>' . $test['user_name'] . '</td>';
         print '<td>' . $test['list_name'] . '</td>';
         print '<td>' . $test['item_text'] . '</td>';
-        print '<td>' . ($test['item_due_date'] ? $test['item_due_date'] : 'None Set') . '</td>';
+        printDueDate($test['item_due_date']);
         print '</tr>';
     }
     print '</table>';
