ef08cab23b584556415a811b3396083c048a1a73
Samuel-Graham <samgraham@outlook.com>
2015-02-16 19:43:17 -0700

    Added in an error page

---
+++ errorHorror.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+<?php
+require 'head.php';
+?>
+</head>
+<body class="container">
+<h1 class="label label-danger">Error, recheck your username and/or password.</h1>
+</body>
+</html>
