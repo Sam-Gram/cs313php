7bc76e3f4c72194363577eea62faf708dbf43157
Samuel-Graham <samgraham@outlook.com>
2015-02-16 19:41:41 -0700

    Added in a way to delete items

---
+++ deleteItem.php
@@ -0,0 +1,17 @@
+<?php
+require 'dbConnection.php';
+require 'queries.php';
+$db = loadDatabase();
+//$q_deleteItem = $q_deleteItem . ' AND ' . ( is_null($_POST['itemduedate']) ? 'i.item_due_date IS NULL' : 'i.item_due_date = :itemduedate');
+$stmt = $db->prepare($q_deleteItem);
+//. (is_null($_POST['itemduedate']) ? 'i.item_due_date IS NULL' : 'i.item_due_date = :itemduedate')
+$stmt->bindParam(':password', $_POST['password']);
+$stmt->bindParam(':username', $_POST['username']);
+$stmt->bindParam(':listname', $_POST['listname']);
+$stmt->bindParam(':itemname', $_POST['itemname']);
+if ($stmt->execute()) {
+    echo json_encode(true);
+} else {
+    echo json_encode(false);
+}
+?>
